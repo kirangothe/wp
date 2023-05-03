b989a5a57d98579d1f2223f8431b377086785757
Kiran Gothe <keyrun.gothe@gmail.com>
2023-05-03 21:53:45 +0545

    test

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'kiran' );
 
 /** Database hostname */
-define( 'DB_HOST', 'my-mysql' );
+define( 'DB_HOST', 'my-mysql:3306' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
